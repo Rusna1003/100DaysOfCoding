8a3ff58f585abd2ce8b42926cdeac3d478114893
Rusna1003 <rusnar784@gmail.com>
2024-09-03 07:45:51 +0700

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,12 @@
+
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Adham";
+        byte umur = 19;
+        String alamat = "Laliko";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Alamat : " + alamat);
+    }
+}
