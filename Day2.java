e3217872b87d3f1da400be1a77868c3c56570a6c
Rusna1003 <rusnar784@gmail.com>
2024-09-03 20:11:00 +0700

    Update Day2.java
    Method  Main

--- Day2.java
+++ Day2.java
@@ -1,12 +1,6 @@
 
 public class Day2 {
     public static void main(String[] args) {
-        String nama = "Adham";
-        byte umur = 19;
-        String alamat = "Laliko";
         
-        System.out.println("Nama : " + nama);
-        System.out.println("Umur : " + umur);
-        System.out.println("Alamat : " + alamat);
     }
 }
