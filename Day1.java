808cfccb14a97db80a9a7bb22ea99a377df80c0b
Rusna1003 <rusnar784@gmail.com>
2024-09-04 14:19:35 +0700

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,8 +1,5 @@
 
 public class Day1 {
-    public static void main(String[] args) {
-        System.out.println("nama: rusna");
-        System.out.println("alamat: pelitakan"); 
-        System.out.println("asal sekolah :smkn tapango");
-    }
+    
+    
 }
