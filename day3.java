7fce9679724950eae49d33ed9239b6a7189a8124
Rusna1003 <rusnar784@gmail.com>
2024-09-04 14:17:54 +0700

    Add files via upload
    system.out.println();

---
+++ day3.java
@@ -0,0 +1,8 @@
+
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println(" nama = rusna");
+            System.out.println(" nim = D0224005");
+            System.out.println(" tujuan masuk informatika = karan saya tertarik pada teknologi dan komputer ");
+    }
+}
