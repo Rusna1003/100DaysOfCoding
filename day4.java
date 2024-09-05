b2326051582b381b0d159aa6ffec44621d3cc8c7
Rusna1003 <rusnar784@gmail.com>
2024-09-05 12:16:52 +0700

    Day4 - Menampilkan  print dan printf

--- day4.java
+++ day4.java
@@ -1,1 +1,14 @@
 
+package day4;
+public class day4 {
+    public static void main(String[] args ){
+            System.out.print("\nnama : rusna");
+            System.out.print("\nalamat : pelitakan ");
+            System.out.print("\nalumni : smk tapango");
+            System.out.printf("\nprodi : %s ", "informatika");
+            System.out.printf("\numur : %d tahun", 19);
+            System.out.printf("\njenis kelamin : %c ", 'P' );
+            
+    }
+}
+
