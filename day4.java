5b13d2dc5d9b819d5a56e220aaf3489386a6f981
Rusna1003 <rusnar784@gmail.com>
2024-09-05 11:46:25 +0700

    menampilkan print dan printf

---
+++ day4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day4;
+
+/**
+ *
+ * @author ACER
+ */
+public class day4 {
+    public static void main(String[] args ){
+            System.out.print("nama saya rusna");
+            System.out.printf("\nprodi %s \numur %d tahun", " informatika",19);
+    }
+}
