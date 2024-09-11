5ee6c5117cb39bd1258087669d07d0606632a219
Rusna1003 <rusnar784@gmail.com>
2024-09-11 14:20:16 +0700

    Update Day3-System.out.println().java
    System.out.println();

--- Day3-System.out.println().java
+++ Day3-System.out.println().java
@@ -1,5 +1,4 @@
-
-public class day3 {
+public class Day3 {
     public static void main(String[] args) {
         System.out.println(" nama = rusna");
             System.out.println(" nim = D0224005");
