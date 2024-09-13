3ff4bc62ec923cd99f5ec80458c6b576094fb33d
Rusna1003 <rusnar784@gmail.com>
2024-09-13 19:35:48 +0700

    Create Day12.java
    int

---
+++ Day12.java
@@ -0,0 +1,13 @@
+
+package Day12;
+
+
+public class java {
+    public static void main(String[] args) {
+        int minimum = -2147483;
+        int  maximum = 2147483;
+        System.out.println("minimum :" + minimum);
+        System.out.println (" maksimum :"+ maximum);
+    }
+    
+}
