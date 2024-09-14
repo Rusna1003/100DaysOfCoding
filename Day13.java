ca377e8c0abd60efe2c6ebbd569116b5614fc8f9
Rusna1003 <rusnar784@gmail.com>
2024-09-14 08:59:12 +0700

    Create Day13.java
    Evaluasi

---
+++ Day13.java
@@ -0,0 +1,28 @@
+
+public class Evaluasi {
+    public static void main(String[] args) {
+        // biodata mahasiswa 1
+        System.out.println(" nama = rusna");
+        System.out.println(" umur = 19 tahun");
+        int umurnenek = 100;
+        System.out.println(" umurnenek:"+ umurnenek);
+        short umurkakek =120;
+        System.out.println(" umurkakek :"+umurkakek);
+        
+        
+        // biodata 2
+        System.out.println(" nama = budi");
+        System.out.println(" umur = 20");
+        
+        // sesudah di update
+         umurnenek = 101;
+        System.out.println(" umurnenek :"+umurnenek);
+        umurkakek = 118;
+        System.out.println(" umurkakek :"+umurkakek);
+        
+        
+        
+    }
+    
+    
+}
