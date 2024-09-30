b6cdfd883afa10125b3581ff422f584d8ecd5acf
Rusna1003 <rusnar784@gmail.com>
2024-09-30 20:41:14 +0800

    Day29.java
    Type data Biginteger

---
+++ Day29.java
@@ -0,0 +1,8 @@
+mport java.math.BigInteger;
+public class Main {
+	public static void main(String[] args) {
+		BigInteger Nik = new BigInteger ("34578102");
+		System.out.println("Nik :"+ Nik);
+		 
+	}
+}
