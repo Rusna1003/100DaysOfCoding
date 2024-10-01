8ffaec4f4ad8196ca561844bf17d276dbae0d740
Rusna1003 <rusnar784@gmail.com>
2024-10-01 22:06:58 +0800

    Day30.java
    Type data BigDecimal

--- Day30.java
+++ Day30.java
@@ -1,7 +1,11 @@
 import java.math.BigDecimal;
 public class Day30 {
 	public static void main(String[] args) {
-		BigDecimal Angka = new BigDecimal ("10032005");
-		System.out.println ("Angka :"+Angka);
+		
+		BigDecimal Angka1 = new BigDecimal ("15.27579845336"); 
+		BigDecimal Angka2= new BigDecimal ("12.48273");
+		// penjumlahan
+		BigDecimal nilai = Angka1. add (Angka2);
+		System.out.println("Penjumlahan :"+ nilai );
 	}
 }
