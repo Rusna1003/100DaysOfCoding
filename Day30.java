03b7a049de061a3f4d3f9d87c51a42ef4b290030
Rusna1003 <rusnar784@gmail.com>
2024-10-01 21:31:35 +0800

    Day30.java
    Type data BigDecimal

---
+++ Day30.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal;
+public class Day30 {
+	public static void main(String[] args) {
+		BigDecimal Angka = new BigDecimal ("10032005");
+		System.out.println ("Angka :"+Angka);
+	}
+}
