ab51bb09672ecb47b9bd6c0d6311c24f6470852e
Rusna1003 <rusnar784@gmail.com>
2024-10-03 20:44:42 +0800

    Day32.java
    Konversi type data primitif ke String

---
+++ Day32.java
@@ -0,0 +1,11 @@
+public class Day32 {
+	public static void main(String[] args) {
+		// Bilangan bulat
+		byte a = 10;
+		String Rusna =Byte.toString(a);
+		System.out.println(Rusna);
+		
+		
+		
+	}
+}
