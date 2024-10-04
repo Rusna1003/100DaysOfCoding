59a9baa23b5fc0654cd3357fb6472de094535d83
Rusna1003 <rusnar784@gmail.com>
2024-10-04 22:30:29 +0800

    Day33.java
    Type casting

---
+++ Day33.java
@@ -0,0 +1,12 @@
+
+public class Day33{
+		public static void main(String[] args) {
+		    
+		    double Rusna = 100.0;
+		    int Unn =(int) Rusna;
+		    
+		    System.out.println("100.0 :"+ Unn);
+		    
+		
+	}
+}
