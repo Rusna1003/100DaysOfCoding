f993241959a4edd83e8b0dc87e90688e53822017
Rusna1003 <rusnar784@gmail.com>
2024-10-05 15:21:15 +0700

    Create Day034.java
    input dari keyboard (Scanner)

---
+++ Day034.java
@@ -0,0 +1,20 @@
+import java.util. Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner Rusna = new Scanner (System.in);
+		 
+		 System.out.print ("Masukkan nama : ");
+		 String nama = Rusna.nextLine();
+		 
+		 System.out.print ("Masukkan angka: ");
+		 byte angka = Rusna.nextByte();
+		 
+		 System.out.print("Masukkan panjang: ");
+		 long panjang = Rusna.nextLong();
+		 
+		 
+		 
+		 
+		 
+	}
+}
