2246b1315aea62e1a84134f30123dd407e23d8e4
Rusna1003 <rusnar784@gmail.com>
2024-10-06 21:02:34 +0800

    Day35.java
    Operator aritmatika = penjumlahan & pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+public class Day35{
+	public static void main(String[] args) {
+		int A,B,C,D,F,G,H,I;
+		A = 10;
+		B=20;
+		C=30;
+		D=10+20+30;
+		System.out.println(A +" + "+ B + " + "+ C+" = " +D);        F=30;
+		G=20;
+		H=10;
+		I=F-G-H;
+		System.out.println(F+"-"+G+"-"+H+"="+I);
+	
+}
+}
