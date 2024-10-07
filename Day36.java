ae40c65b3d1ffff14a05a683273a9a5409ffb4be
Rusna1003 <rusnar784@gmail.com>
2024-10-07 21:11:06 +0800

    Day36.java
    Pembagian,perkalian,sisah bagai

---
+++ Day36.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		// perkalian
+		int a = 10;
+		int b=10;
+		int c = a*b;
+		System.out.println(c);
+		// pembagian
+	int d = 30;
+    int e =2;
+    int f =d/e;
+    System.out.println(f);
+    // Sisah bagi
+   int g =10;
+  int h=5;
+  int i=g%h;
+  System.out.println(i);
+	}  
+}
