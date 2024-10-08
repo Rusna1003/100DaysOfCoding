25dbac780e39c642d87c0ddb6b51ac45fc7e8243
Rusna1003 <rusnar784@gmail.com>
2024-10-08 21:24:14 +0800

    Day37.java
    Pengisian nilai , penambahan dan pengurangan

---
+++ Day37.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int RusnaImutt = 30;
+		System.out.println("RusnaImutt :"+ RusnaImutt);
+		
+		RusnaImutt +=10;
+		System.out.println("Penambahan : ");
+		System.out.println("RusnaImutt :"+RusnaImutt);
+		
+		RusnaImutt -= 10;
+		System.out.println("Pengurangan :");
+		System.out.println("RusnaImutt :"+RusnaImutt);
+	}
+}
