b402c44b3851c6761cbf33f5bb522f12bc769b81
Rusna1003 <rusnar784@gmail.com>
2024-10-09 21:24:19 +0800

    Day38.java
    Pengoperasian perkalian dan pembagian

---
+++ Day38.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		int Rusna= 20;
+		System.out.println("Rusna:"+Rusna);
+		 
+		 Rusna *= 2;
+		 System.out.println("Perkalian:");
+		 System.out.println("Rusna :"+Rusna);
+		 
+		 Rusna /= 2;
+		 System.out.println("Pembagian ");
+		 System.out.println("Rusna :"+Rusna);
+		
+		
+	}
+}
