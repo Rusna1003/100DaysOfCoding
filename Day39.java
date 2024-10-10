14be3277a922d1cc0b9cd364fd19679cd16d072f
Rusna1003 <rusnar784@gmail.com>
2024-10-10 19:54:00 +0800

    Day39.java
    Operator penugasan: pengisian nilai modulus /sisah bagi

---
+++ Day39.java
@@ -0,0 +1,22 @@
+mport java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner Q= new Scanner(System.in);
+		int a,b,c;
+		
+		System.out.print("Masukkan nilai  :");
+		a =Q.nextInt();
+		
+		System.out.print("Masukkan nilai   :");
+		b=Q.nextInt();
+		c=a%b;
+		System.out.println("Sisah  baginya  adalah :"+c);
+		
+	
+	
+        
+		
+	
+		
+	}
+}
