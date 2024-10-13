ee7dbdd6dddd4bf8c1ae75652253b70606c5542d
Rusna1003 <rusnar784@gmail.com>
2024-10-13 20:59:18 +0800

    Day42.java
    Operator perbandingan == dengan & != Sama dengan

---
+++ Day42.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int a,b;
+		a= 10;
+		b= 20;
+		System.out.println("Apakah nilai a sama dengan b  : " + ( a == b));
+		System.out.println("Apakah nilai a dan b tidak sama  : "+ ( a != b));
+	}
+}
