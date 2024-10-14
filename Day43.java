3993c9a0400350900b41bd0d7122e463e5b0e159
Rusna1003 <rusnar784@gmail.com>
2024-10-14 19:54:41 +0800

    Day43.java
    Operator perbandingan <= lebih kecil sama dengan
                                              >= Lebih besar sama dengan

---
+++ Day43.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int r,u;
+		r = 45;
+		u = 30;
+		System.out.println("Apakah nilai r lebih besar dari u :"+  (r>=u));
+		System.out.println("Apakah nilai r lebih kecil dari u :"+ (r<=u));
+	}
+}
