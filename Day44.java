c0b9eed385fb42e9cda9e5b6a06f2365cd22013b
Rusna1003 <rusnar784@gmail.com>
2024-10-15 22:33:54 +0800

    Day44.java
    Operator logika and &&

--- Day44.java
+++ Day44.java
@@ -1,26 +1,8 @@
-package Day44.java;
-
-import java.util.Scanner;
-
-
-public class Rusna {
-
-        public static void main(String[] args) {
-       Scanner rusna = new Scanner(System.in);
-            System.out.println("masukan angka pertama");
-       int a = rusna.nextInt();
-            System.out.println("masukan angka kedua");
-       int b = rusna.nextInt();
-       
-       if(a>=b && a<=b){
-           System.out.println("angka a dan b sama besar");
-       }
-       else if(a>b){
-           System.out.println("angka a lebih besar dari b");
-       }
-       else if(a<b){
-           System.out.println("angka a lebih kecil dari b ");
-       }
-    }
-    
+public class Main {
+	public static void main(String[] args) {
+		boolean a,b;
+		a = true;
+		b = false;
+		System.out.println( a && b);
+	}
 }
