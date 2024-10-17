5275181bea9ac2fe7fe4c86a7454840a007c7603
Rusna1003 <rusnar784@gmail.com>
2024-10-17 20:09:02 +0800

    Day46.java
    Operator logika = negasi/kebalikan !

---
+++ Day46.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean Angka= true;
+		boolean b = !Angka;
+		System.out.println( "Angka awal :" +Angka);
+		System.out.println("Hasil negasi :"+b);
+	}
+}
