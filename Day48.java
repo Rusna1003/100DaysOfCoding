990fb92884f0f3ccceca458e1d45691ed317e144
Rusna1003 <rusnar784@gmail.com>
2024-10-19 00:15:55 +0800

    Day48.java
    Operator percabangan if,else

---
+++ Day48.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int b = 4;
+		if( b < 2 ){
+		    System.out.println( "Nilai b < dari 2");
+	} else {
+	    System.out.println( "Nilai b > dari 2");
+}
+}
+}
