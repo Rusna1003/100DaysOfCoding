456cf904c2dda8246a64f330a8a6fc93e6a28fde
Rusna1003 <rusnar784@gmail.com>
2024-10-21 21:37:11 +0800

    Day50.java
    Operator percabangan if,else if,else

---
+++ Day50.java
@@ -0,0 +1,16 @@
+public class main {
+	public static void main(String[] args) {
+		int a = 10;
+		
+		if ( a < 2){
+		    System.out.println( "a lebih kecil dari 2");
+		    
+	}else if ( a > 12 ){
+	    System.out.println( "a lebih besar dari 12");
+	    
+	    
+}else {
+    System.out.println( "a lebih besar dari 3");
+}
+	}
+}
