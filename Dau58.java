42fd534ac2d0158134c72f71586a61e83bb0faa4
Rusna1003 <rusnar784@gmail.com>
2024-10-29 22:23:17 +0800

    Day58.java
    While decrement --

---
+++ Dau58.java
@@ -0,0 +1,12 @@
+public class whileDecrement {
+    public static void main(String[] args) {
+        // decrement --
+        int a = 5;
+        while (a > 0){
+        System.out.println(a);
+        a--;
+                
+        
+    }
+    
+}
