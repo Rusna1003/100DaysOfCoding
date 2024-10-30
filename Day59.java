0fc9cd15fc720619c36ce0ab5027339a52ac16df
Rusna1003 <rusnar784@gmail.com>
2024-10-30 21:31:02 +0800

    Day59.java
    Looping for each

---
+++ Day59.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		  int [ ] a=  {1,2,3,4,5};
+		  for(int angka : a ){
+		      System.out.println(angka);
+		  System.out.println(" ");
+	}
+	}
+}
