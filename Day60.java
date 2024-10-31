eaf2af31dbc4b496f55bdf628865c58790217f0f
Rusna1003 <rusnar784@gmail.com>
2024-10-31 22:20:04 +0800

    Day60.java
    Doo while

---
+++ Day60.java
@@ -0,0 +1,10 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println(i);  // Cetak nilai i
+            i++;                    // Tambah i dengan 1
+        } while (i <= 5);            // Cek kondisi i <= 5
+    }
+}
