f959e03c1403d67e7adf1a5970d087f891f30e43
Rusna1003 <rusnar784@gmail.com>
2024-11-02 18:00:54 +0800

    Day62.java
    Keywoard break

---
+++ Day62.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println("Loop berhenti pada angka: " + i);
+                break; // maka akan berhenti di angka 5
+            }
+            System.out.println("Angka: " + i);
+        }
+        
+    }
+}
+	
