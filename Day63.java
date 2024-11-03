101bfe3b2c55a6a964c95ef48bdbcaa1d849a213
Rusna1003 <rusnar784@gmail.com>
2024-11-03 20:32:37 +0800

    Day63.java
    Keywoard continue 
    Hanya untuk melewati angka" di kondisi tertentu seperti di kelipatan

---
+++ Day63.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		for(int a =1; a<=15; a++ ){
+		    if (a % 3 ==0){
+		        continue;
+		        
+	}
+	System.out.println(a);
+}
+	}
+}
