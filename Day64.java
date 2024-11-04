ff500cdc2d1225c6f1b8c8a51cc24b7a8df8577d
Rusna1003 <rusnar784@gmail.com>
2024-11-04 21:56:52 +0800

    Day64.java
    Break label

---
+++ Day64.java
@@ -0,0 +1,15 @@
+package DaysOfCoding;
+
+
+public class Days64 {
+    public static void main(String[] args) {
+        Rusna:
+        for( int a =1; a <= 10; a++){
+            if (a== 5){
+                break Rusna;
+                
+        }System.out.println(a);
+        }
+        
+    }
+}
