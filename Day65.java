3c232bfd6e66ee901a68c065644b20715171038c
Rusna1003 <rusnar784@gmail.com>
2024-11-05 22:35:14 +0800

    Day65.java
    Nested loop = perulangan dalam perulangan

---
+++ Day65.java
@@ -0,0 +1,9 @@
+public class NestedLoopExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {  // Loop luar
+            for (int j = 0; j < 3; j++) {  // Loop dalam
+                System.out.println("i: " + i + ", j: " + j);
+            }
+        }
+    }
+}
