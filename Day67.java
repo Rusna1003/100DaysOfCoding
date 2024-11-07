e25011da5a5cc24e0676a63d1391bab11f19f67a
Rusna1003 <rusnar784@gmail.com>
2024-11-07 21:53:57 +0800

    Day67.java
    Method void

---
+++ Day67.java
@@ -0,0 +1,9 @@
+public class rusna {
+    public static void cetakPesan() {
+        System.out.println("Rusna calon programmeer");
+    }
+
+    public static void main(String[] args) {
+        cetakPesan();
+    }
+}
