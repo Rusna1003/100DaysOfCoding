e94489a366ce1c5e337290605738686ed2dc6527
Rusna1003 <rusnar784@gmail.com>
2024-11-08 22:47:39 +0800

    Day68.java
    Method non void

---
+++ Day68.java
@@ -0,0 +1,14 @@
+package method_nonvoid;
+
+
+class Day68 {
+    public static void main(String[] args) {
+        
+   
+       Day68 luppy = new Day68();
+        System.out.println(luppy.nama());
+    }
+     String nama(){
+        return "luppy";
+    } 
+    }
