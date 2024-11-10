7be374d59d5e6131e821105285b82070126df6d0
Rusna1003 <rusnar784@gmail.com>
2024-11-10 19:13:04 +0800

    Day70.java
    Method parameter

---
+++ Day70.java
@@ -0,0 +1,9 @@
+public class day070 {
+    public static String lupyy (String nama,int umur){
+        return nama + " berumur "+umur +" tahun";
+    }
+    public static void main(String[] args) {
+        System.out.println(lupyy("Rusna",18));
+    }
+    
+}
