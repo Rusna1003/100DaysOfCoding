9c960243ddd93856d60b13b9c777ff3942b0de34
Rusna1003 <rusnar784@gmail.com>
2024-11-13 22:58:36 +0800

    Day73.java
    Char art indeks

---
+++ Day73.java
@@ -0,0 +1,11 @@
+		public class day73 {
+    public static void main(String[]args){
+        String nama = "Rusna";
+        
+        System.out.print(nama);
+        System.out.print(" char[2] adalah "+nama.charAt(2));
+        
+    }
+    
+}
+	
