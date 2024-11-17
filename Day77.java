cf76b22d9d62c91762fd5552e7535551e3093eb8
Rusna1003 <rusnar784@gmail.com>
2024-11-17 20:41:04 +0800

    Day77.java
    equals= membandikan nilai

---
+++ Day77.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String nama1 = "Rusna";
+		String nama2 ="Rusna";
+		String nama3 ="rusna";
+		System.out.println(" nama1 equals nama2:"+nama1. equals (nama2));
+		System.out.println("nama1 equals nama3 :" + nama1. equals (nama3));
+	}
+}
