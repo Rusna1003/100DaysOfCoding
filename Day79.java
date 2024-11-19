c1db73c543490ef672535998ce8e1bff4bd3c4de
Rusna1003 <rusnar784@gmail.com>
2024-11-19 20:58:22 +0800

    Day79.java
    String method to string

---
+++ Day79.java
@@ -0,0 +1,21 @@
+package Day79;
+
+
+public class java {
+    public static void main(String[] args) {
+        int a = 79;
+        String b = Integer.toString(a);
+        System.out.println(b);
+    }
+    
+}package Day79;
+
+
+public class java {
+    public static void main(String[] args) {
+        int a = 79;
+        String b = Integer.toString(a);
+        System.out.println(b);
+    }
+    
+}
