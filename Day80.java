8e07f1d0df947e456c0789eeca3f7543964d8759
Rusna1003 <rusnar784@gmail.com>
2024-11-21 00:14:42 +0800

    Day80.java
    String method trim

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+
+		String nama ="      Rusna   ";
+		System.out.println("sebelum di trim :" + nama);
+		System.out.println( "sesudah di trim :"+nama.trim() );
+		
+	}
+}
